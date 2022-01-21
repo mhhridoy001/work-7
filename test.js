6e59dabbcdee7be5814f8506a23daa7137ece907
mehedihasanhridoy <mehedihasanhridoy543@gmail.com>
2022-01-22 00:19:27 +0600

    Statement/Syntext

---
+++ test.js
@@ -0,0 +1,30 @@
+// JavaScript Statements start.
+let x, y, z;  // Statement 1
+x = 5;        // Statement 2
+y = 6;        // Statement 3
+z = x + y;    // Statement 4
+document.getElementById("demo").innerHTML =
+"The value of z is " + z + ".";
+
+document.getElementById("demo1").innerHTML = "Hello Dolly.";
+
+let a, b, c;
+a = 5;
+b = 6;
+c = a + b;
+document.getElementById("demo2").innerHTML = 
+"The value of c is " + c + ".";
+
+// JavaScript Statements end.
+
+// JavaScript Syntax start
+let e , f;
+e = 6;
+f= 8;
+document.getElementById("assign").innerHTML = e + f;
+
+document.getElementById("expression").innerHTML = 5 * 6;
+
+document.getElementById("string").innerHTML = "Mehedi" + " "+ "Hridoy";
+
+// JavaScript Syntax end
